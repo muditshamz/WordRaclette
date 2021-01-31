8631693ebd445c64ef46d523f8f1911fd20a1f01
Mudit Sharma <muditshamz@gmail.com>
2021-01-31 10:46:33 +0530

    Initial Commit

---
+++ Shared/WordRacletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordRacletteApp.swift
+//  Shared
+//
+//  Created by Mudit Sharma on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct WordRacletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
