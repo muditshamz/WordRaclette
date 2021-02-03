301ceac03fce77ba442c09d9a2157716daac8635
Mudit Sharma <muditshamz@gmail.com>
2021-02-03 23:11:59 +0530

    MatchedGeometry effect on card || Preparation work for many cards

--- Shared/CourseView.swift
+++ Shared/CourseView.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct CourseView: View {
+    @State var ShowFullCard = false
+    @Namespace var namespaceCard
     var body: some View{
-        Text("")
+        ZStack {
+            CourseItem()
+                .matchedGeometryEffect(id: "Card", in: namespaceCard, isSource: !ShowFullCard)
+                .frame(width: 335, height: 250, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            if ShowFullCard {
+                ScrollView {
+                    CourseItem()
+                        .matchedGeometryEffect(id: "Card", in: namespaceCard)
+                        .frame(height: 300)
+                    
+                    VStack {
+                        ForEach(0 ..< 20) { item in
+                            CourseRow()
+                        }
+                    }
+                    .padding()
+                }
+                .transition(.opacity)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            }
+        }
+        .onTapGesture {
+            //This solves the problem of animation delay
+            withAnimation(.spring()) {
+                ShowFullCard.toggle()
+            }
+        }
+        //        .animation(.spring()) this delays the animation and back card get visible
     }
     
 }
